3ca5536579e030d08490bcd34b56bde3ad598435
Devika Sajeesh <73882104+Devika-Sajeesh@users.noreply.github.com>
2026-01-31 19:54:37 +0530

    Implement BooleanMatrix function and main method
    Implemented the BooleanMatrix function to modify the matrix based on the presence of 1s in each row. Added main method to read input and print the modified matrix.

--- src/BooleanMatrix.java
+++ src/BooleanMatrix.java
@@ -1,14 +1,53 @@
-public class BooleanMatrix {
-    
-    // TODO: Implement the function to modify the matrix
-    
-        // Logic: Check each row. If it contains a 1, set the whole row to 1.
-    
+import java.util.Scanner;
 
-    
-        // TODO: Read m and n
-        // TODO: Read the matrix elements
-        // TODO: Call BooleanMatrixProblem(mat, m, n)
-        // TODO: Print the modified matrix
-    
+public class BooleanMatrix {
+
+    static void BooleanMatrix(int[][] mat, int m, int n) {
+
+        for (int i = 0; i < m; i++) {
+            boolean hasOne = false;
+
+            // Check if row i contains a 1
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    hasOne = true;
+                    break;
+                }
+            }
+
+            // If row has a 1, fill entire row with 1s
+            if (hasOne) {
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        BooleanMatrixProblem(mat, m, n);
+
+        // Print result
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
